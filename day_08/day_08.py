dc8f7531a6714496cf245a7d9e0f9e2b228128f7
Kenny Jeong <kjeong@sorensonmedia.com>
2020-12-08 03:27:43 +0900

    8th day problem solving https://www.hackerrank.com/challenges/30-linked-list/problem

---
+++ day_08/day_08.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# The problem is coming from https://www.hackerrank.com/challenges/30-linked-list/problem
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+    #Complete this method
+        if head == None:
+            return(Node(data))
+        else:
+            current = head
+            while current.next:
+                # Find Termination of node
+                current = current.next
+            current.next = Node(data)
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+mylist.display(head);
