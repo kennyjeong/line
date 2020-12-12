109cf3269b1990683e066af7985028b8091005f4
Kenny Jeong <kjeong@sorensonmedia.com>
2020-12-13 03:04:38 +0900

    Day 13 Problem solving, https://www.hackerrank.com/challenges/30-generics/problem

---
+++ day_13/day_13.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+// Day 13 Problem Solving, coming from https://www.hackerrank.com/challenges/30-generics/problem
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+template <class T>
+void printArray (vector<T> a) {
+    for(int i=0; i<a.size();i++) {
+        cout << a[i] << endl;
+    }
+}
+
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
+}
+
