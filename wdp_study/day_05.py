1c18fcb806b52bbde24d0bea03a9db6b1ee21d94
Kenny Jeong <kjeong@sorensonmedia.com>
2021-01-04 00:31:11 +0900

    Day 05, coming from https://www.hackerrank.com/challenges/minimum-swaps-2/

---
+++ wdp_study/day_05.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    swaps=0
+    for i in range(0,n):
+        while arr[i] != (i+1):
+            temp = arr[i]-1
+            arr[i], arr[temp] = arr[temp], arr[i]
+            swaps+=1
+    return swaps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
+
