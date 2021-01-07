58f49bffbcd081db9d9050ec436e28e526c5d1f7
Kenny Jeong <kjeong@sorensonmedia.com>
2021-01-08 00:49:55 +0900

    Day 08 Problem Solving, coming from https://www.hackerrank.com/challenges/alternating-characters

---
+++ wdp_study/day_08.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#Day 08 Problem Solving, coming from https://www.hackerrank.com/challenges/alternating-characters
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    count = 0
+    for num in range (len(s)-1):
+        if s[num] == s[num+1]:
+            count +=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
