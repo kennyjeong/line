ef40968f24346b625f709c0982ba81d803573f26
Kenny Jeong <kjeong@sorensonmedia.com>
2021-01-13 04:31:04 +0900

    Day 12 Problem Solving, coming from https://www.hackerrank.com/challenges/ctci-bubble-sort/

---
+++ wdp_study/day_12.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#Day 12 Problem Solving, coming from https://www.hackerrank.com/challenges/ctci-bubble-sort/
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+    numSwaps = 0
+
+    while True:
+        SwapsFlag = False
+        for i in range(len(a)-1):
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                numSwaps += 1
+                SwapsFlag = True
+        if not SwapsFlag:
+            break
+
+    print('Array is sorted in', numSwaps, 'swaps.')
+    print('First Element:', a[0])
+    print('Last Element:', a[-1])
+
+    return
+
+if __name__ == '__main__':
+    n = int(input())
+    a = list(map(int, input().rstrip().split()))
+    countSwaps(a)
+
