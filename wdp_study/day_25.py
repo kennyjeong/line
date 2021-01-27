d3e7d7fe43eff1a76c34a92368952db20b9d314d
kennyjeong <kenny.jeong@linecorp.com>
2021-01-28 00:33:21 +0900

    Day 25 Problem Solving, Coming from https://www.hackerrank.com/challenges/pairs

---
+++ wdp_study/day_25.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#Day 25 Problem Solving, Coming from https://www.hackerrank.com/challenges/pairs
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the pairs function below.
+def pairs(k, arr):
+    myDict = {item : 1 for index, item in enumerate(arr)}
+    count = 0
+    for each in myDict:
+        if each + k in myDict:
+            count += 1
+    print count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
